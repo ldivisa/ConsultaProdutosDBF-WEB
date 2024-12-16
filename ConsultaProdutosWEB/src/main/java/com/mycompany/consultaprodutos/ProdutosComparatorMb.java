7f8f9dde68c2ce6c9f660c6aa64377ee4e00aec0
ldivisa <usocomercial@gmail.com>
2024-12-16 00:31:38 -0300

    2024-12-16 B - Arrumando ordenacao por margem para pegar do maior lucro ao menor

--- ConsultaProdutosWEB/src/main/java/com/mycompany/consultaprodutos/ProdutosComparatorMb.java
+++ ConsultaProdutosWEB/src/main/java/com/mycompany/consultaprodutos/ProdutosComparatorMb.java
@@ -14,8 +14,8 @@ public class ProdutosComparatorMb implements Comparator<Produtos>{
 
     @Override
     public int compare(Produtos o1, Produtos o2) {
-        int p1 = (int) Math.round((o1.getPreco()-o1.getCusto())*100);
-        int p2 = (int) Math.round((o2.getPreco()-o1.getCusto())*100);
+        int p1 = (int) Math.round(o1.getPreco()-o2.getCusto())*100;
+        int p2 = (int) Math.round(o1.getPreco()-o2.getCusto())*100;
         
         return (p2-p1);
     }
